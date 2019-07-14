f6c2a509befac375e8d0e2a6fffae736793c9431
Tun Han Mra <imtunhanmra@gmail.com>
2019-07-14 19:24:29 +0630

    Remove validates from user for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,6 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtBlacklist
 
-  validates :name,
-      presence: true,
-      length: {minimum: 3}
-
-  validates :user_name,
-      presence: true,
-      uniqueness: true,
-      length: {minimum: 3}
-
-  validates :contact_number,
-      presence: true,
-      length: {maximum: 15,minimum: 6}
-
   validates :user_type, :inclusion => { :in =>  USER_TYPES }, :allow_nil => true, :allow_blank => true
 
   has_many :user_roles, dependent: :destroy, class_name: "UserRole"
