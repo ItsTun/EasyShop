05f294c079ccf992066a64f0bc29d61164e17116
Tun Han Mra <imtunhanmra@gmail.com>
2019-07-19 13:39:46 +0630

    Change api only app

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Shop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.api_only = true
     config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec, fixture: true
