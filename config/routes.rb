2c700e018750416492e42403683a7af1856647dc
Tun Han <tunhanmra@rebbiz.com>
2020-09-28 11:56:06 +0630

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   mount RailsEmailPreview::Engine, at: 'emails'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'home#index'
   devise_for :users
   draw :v1_routes
 end
